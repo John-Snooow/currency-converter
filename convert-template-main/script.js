e55d75789e27e26fb309434c72d113f41a1f755a
John Frank <139596702+John-Snooow@users.noreply.github.com>
2024-12-09 22:30:13 -0300

    Catando o evento de submit (enviar) do formulário

--- convert-template-main/script.js
+++ convert-template-main/script.js
@@ -1,7 +1,16 @@
+//obitendo os elementos do foemulário.
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 //manipulando o input amount para recerber somente números. 
 amount.addEventListener("input", ()=>{
     const hasCharactersRegex = /\D+/g
     amount.value = amount.value.replace(hasCharactersRegex, "")
 })
+
+//Catando o evento de submit (enviar) do formulário.
+form.onsubmit = (event) =>{
+    event.preventDefault()// eveita que a pagina tenha o carregamente padrão 
+    console.log(currency.value)
+}
